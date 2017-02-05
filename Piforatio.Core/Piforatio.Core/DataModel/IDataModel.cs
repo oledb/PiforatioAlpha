6b9915a571dc87d83a31919afa27237bd657bd9f
oledb <zf0qwe@gmail.com>
2017-02-05 19:43:30 +0300

    Add ProjectModel class

--- Piforatio.Core/Piforatio.Core/DataModel/IDataModel.cs
+++ Piforatio.Core/Piforatio.Core/DataModel/IDataModel.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Piforatio.Core.ObjectsAbstract;
 
 namespace Piforatio.Core.DataModel
 {
-    public interface IDataModel
+    public interface IDataModel<T> where T : ICoreObject
     {
-        void Update();
+        IEnumerable<T> GetData(int id);
+        IEnumerable<T> GetAllData();
+        void UpdateAll();
+        void Update(T obj);
     }
 }
---
+++ Piforatio.Core/Piforatio.Core/DataModel/ProjectModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;s
+using Piforatio.Core.ObjectsAbstract;
+
+namespace Piforatio.Core.DataModel
+{
+    public class ProjectModel : IDataModel<IProject>
+    {
+        public IEnumerable<IProject> GetAllData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IProject> GetData(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(IProject obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
