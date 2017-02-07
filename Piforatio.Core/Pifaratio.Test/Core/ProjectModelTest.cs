780e2f4e07f9e65901371c0f9c70620cae97e684
oledb <zf0qwe@gmail.com>
2017-02-07 21:11:18 +0300

    Removed Test\Mocks from Piforatio.Core as unnecessary. GenerateProject and CreateProjectList methods moved to ProjectModelTest class

--- Piforatio.Core/Pifaratio.Test/Core/ProjectModelTest.cs
+++ Piforatio.Core/Pifaratio.Test/Core/ProjectModelTest.cs
@@ -5,17 +5,37 @@ using NUnit.Framework;
 using Moq;
 using Piforatio.Core.ObjectsAbstract;
 using Piforatio.Core.DataModel;
-using Piforatio.Core.Test.Mocks;
 
 namespace Piforatio.Test.Core
 {
     [TestFixture]
     public class ProjectModelTest
     {
+        public static List<IProject> CreateProjectList()
+        {
+            var listObject = new List<IProject>();
+            listObject.AddRange(new IProject[]{
+                GenerateProject("MVC", new DateTime(2017,1,20)),
+                GenerateProject("Point Theory", new DateTime(2017,1,10)),
+                GenerateProject("Xamarin", new DateTime(2017,2,1)),
+            });
+            return listObject;
+        }
+
+        public static IProject GenerateProject(string name, DateTime time)
+        {
+            int _index = 0;
+            var mock = new Mock<IProject>();
+            mock.Setup(p => p.Name).Returns(name);
+            mock.Setup(p => p.CreationTime).Returns(time);
+            mock.Setup(p => p.ProjectID).Returns(_index++);
+            return mock.Object;
+        }
+
         [Test]
         public void Create_success()
         {
-            ProjectModel pm = new ProjectModelMock();
+            ProjectModel pm = new ProjectModel();
             var list = new List<IProject>();
             list = (List<IProject>)pm.GetAllData();
             list.RemoveAll(p => true);
@@ -30,7 +50,7 @@ namespace Piforatio.Test.Core
         [Test]
         public void ctor_LitNotNull()
         {
-            var pm = new ProjectModel
+            var pm = new ProjectModel();
         }
     }
 }
