a2d7ead2af7ccec13d24bc88db879ed8298042c4
oledb <zf0qwe@gmail.com>
2017-02-20 20:15:08 +0300

    Add autodoc for ProjectVMCollection

--- Piforatio.Core/Piforatio.Win/ViewModelCollection/ProjectVMCollection.cs
+++ Piforatio.Core/Piforatio.Win/ViewModelCollection/ProjectVMCollection.cs
@@ -13,8 +13,10 @@ namespace Piforatio.Win.ViewModelCollection
     public class ProjectVMCollection : Notifier
     {
         protected ProjectModel _projectModel;
-        public ProjectVMCollection() { }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="ProjectVMCollection"/> using <see cref="ProjectModel"/>
+        /// </summary>
         public ProjectVMCollection(ProjectModel projectModel)
         {
             _projectModel = projectModel;
@@ -22,9 +24,16 @@ namespace Piforatio.Win.ViewModelCollection
             Projects = new ObservableCollection<IProject>(_projectModel.GetAllProjects());
         }
 
+        /// <summary>
+        /// For data binding only!
+        /// </summary>
         public ObservableCollection<IProject> Projects { get; set; }
 
         protected ProjectVM _selectedProject;
+
+        /// <summary>
+        /// For data binding only! To change this property use <see cref="SelectProjectByValue"/>.
+        /// </summary>
         public ProjectVM SelectedProject
         {
             get
@@ -39,6 +48,12 @@ namespace Piforatio.Win.ViewModelCollection
         }
 
         private int _index = -1;
+
+        /// <summary>
+        /// Changes <see cref="SelectedProject"/>
+        ///<para/>
+        /// Can throw default exceptions of <see cref="ObservableCollection{T}"/>
+        /// </summary>
         public int SelectProjectByValue
         {
             get
@@ -55,6 +70,11 @@ namespace Piforatio.Win.ViewModelCollection
             }
         }
 
+        /// <summary>
+        /// Adds object of <see cref="IProject"/> to the end of <see cref="Projects"/> and sets it in <see cref="SelectedProject"/>
+        /// and <see cref="SelectProjectByValue"/>
+        /// </summary>
+        /// <param name="project"></param>
         public void AddProject(IProject project)
         {
             _projectModel.Update(project, ChangedType.Add);
@@ -62,6 +82,10 @@ namespace Piforatio.Win.ViewModelCollection
             SelectProjectByValue = Projects.Count - 1;
         }
 
+        /// <summary>
+        /// Removes <see cref="SelectedProject"/> and removes it from <see cref="Projects"/>. 
+        /// Sets a <see cref="SelectProjectByValue"/> to -1
+        /// </summary>
         public void RemoveSelectedProject()
         {
             IProject project = (from p in Projects
@@ -75,6 +99,9 @@ namespace Piforatio.Win.ViewModelCollection
 
         }
 
+        /// <summary>
+        /// Updates all fields of <see cref="SelectedProject"/>
+        /// </summary>
         public void SaveSelectedProjectChange()
         {
             _projectModel.Update(SelectedProject, ChangedType.Modify);
