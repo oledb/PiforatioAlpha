fe523d1ceddb3cdccb7ce21c8d40bef448c18612
oledb <zf0qwe@gmail.com>
2017-02-21 20:01:32 +0300

    Add commands to ProjectView

--- Piforatio.Core/Piforatio.Win/View/Panels/DataViewPanels/ProjectsView.xaml.cs
+++ Piforatio.Core/Piforatio.Win/View/Panels/DataViewPanels/ProjectsView.xaml.cs
@@ -6,6 +6,7 @@ using Piforatio.Core.ObjectsAbstract;
 using Piforatio.Win.ViewModelCollection;
 using Piforatio.Win.ViewModel;
 using Piforatio.Win.Fakes;
+using System.Windows.Input;
 
 namespace Piforatio.Win.View.Panels.DataViewPanels
 {
@@ -15,12 +16,48 @@ namespace Piforatio.Win.View.Panels.DataViewPanels
     public partial class ProjectsView : UserControl
     {
         private ProjectVMCollection _projectVMCollection;
+
+        private IProject newProject = null;
+
         public ProjectsView(ProjectVMCollection projectVMCollection)
         {
             InitializeComponent();
             _projectVMCollection = projectVMCollection;
-            DataContext = _projectVMCollection;
-            
+            DataContext = _projectVMCollection;    
+        }
+
+        private RoutedUICommand AddNewProject = new RoutedUICommand("Add new project", "Add new project", typeof(UserControl));
+        private RoutedUICommand SaveNewProject = new RoutedUICommand("Save new project", "Save new project", typeof(UserControl));
+        private RoutedUICommand CancelNewProject = new RoutedUICommand("Cancel new project", "Cancel new project", typeof(UserControl));
+
+        private void AddNewProject_Execute(object sender, ExecutedRoutedEventArgs args)
+        {
+
+        }
+
+        private void AddNewProject_CanExecute(object sender, CanExecuteRoutedEventArgs args)
+        {
+
+        }
+
+        private void SaveNewProject_Execute(object sender, ExecutedRoutedEventArgs args)
+        {
+
+        }
+
+        private void SaveNewProject_CanExecute(object sender, CanExecuteRoutedEventArgs args)
+        {
+
+        }
+
+        private void CancelNewProject_Execute(object sender, ExecutedRoutedEventArgs args)
+        {
+
+        }
+
+        private void CancelNewProject_CanExecute(object sender, CanExecuteRoutedEventArgs args)
+        {
+
         }
     }
 }
