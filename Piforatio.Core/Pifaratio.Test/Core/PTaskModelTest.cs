b7d40e795959426c74b9c4ced3ee4e10bd329db5
oledb <zf0qwe@gmail.com>
2017-02-23 11:43:33 +0300

    Add tests

--- Piforatio.Core/Pifaratio.Test/Core/PTaskModelTest.cs
+++ Piforatio.Core/Pifaratio.Test/Core/PTaskModelTest.cs
@@ -13,8 +13,30 @@ namespace Piforatio.Test.Core
     public class PTaskModelTest
     {
         [Test]
+        public void CreateAndSavePTask_Failed()
+        {
+            //Arrange
+            IDataContextFactory factory = CreateDataContextFabricaStub();
+            PTaskModel model = new PTaskModel(factory, null);
+
+            //Act
+
+            //Assert
+
+            throw new NotImplementedException();
+        }
+
+        [Test]
         public void CreateAndSavePTask()
         {
+            //Arrange
+            IDataContextFactory factory = CreateDataContextFabricaStub();
+            IProject project = CreateProject("Test Project", new DateTime(2017, 1, 20), 34);
+            PTaskModel model = new PTaskModel(factory, null);
+
+            //Act
+
+            //Assert
             throw new NotImplementedException();
         }
     }
