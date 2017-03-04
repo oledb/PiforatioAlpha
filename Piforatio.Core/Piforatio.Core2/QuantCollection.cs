9c6932820f0cfe747665f1e4e27df4e552182bac
oledb <zf0qwe@gmail.com>
2017-03-04 19:32:37 +0300

    Add Quant, QuantCollection

---
+++ Piforatio.Core/Piforatio.Core2/QuantCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piforatio.Core2
+{
+    public class QuantCollection
+    {
+        public void Add(Quant quant)
+        {
+
+        }
+
+        public int Length { get; }
+    }
+}
---
+++ Piforatio.Core/Piforatio.Core2Test/MainTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using Piforatio.Core2;
+
+namespace Piforatio.Core2Test
+{
+    [TestFixture]
+    public class MainTest
+    {
+        [Test]
+        public void AddNewQuant()
+        {
+            //Arrange
+            var quant = new Quant()
+            {
+                Time = new DateTime(2017, 3, 3, 10, 30, 00),
+                Project = "C# Books",
+                Objective = "Piforatio - Logic",
+                Count = 4
+            };
+
+            var quantCollection = new QuantCollection();
+
+            //Act
+            quantCollection.Add(quant);
+
+            //Assert
+            Assert.AreEqual(1, quantCollection.Length);
+        }
+    }
+}
