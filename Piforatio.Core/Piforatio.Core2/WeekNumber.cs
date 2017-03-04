42879415b3d2bb1e9cb65266b3073abe0b25ace8
oledb <zf0qwe@gmail.com>
2017-03-04 21:59:54 +0300

    Add WeekNumber class

---
+++ Piforatio.Core/Piforatio.Core2/WeekNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Piforatio.Core2
+{
+    public static class WeekNumber
+    {
+        // this method is borrowed from http://stackoverflow.com/a/11155102/284240
+        private static int GetIso8601WeekOfYear(DateTime time)
+        {
+            DayOfWeek day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
+            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
+            {
+                time = time.AddDays(3);
+            }
+
+            return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+        }
+
+        public static DateTime FirstDateOfWeek(int year, int weekOfYear)
+        {
+            CultureInfo ci = new CultureInfo("Ru-ru");
+            DateTime jan1 = new DateTime(year, 1, 1);
+            int daysOffset = (int)ci.DateTimeFormat.FirstDayOfWeek - (int)jan1.DayOfWeek;
+            DateTime firstWeekDay = jan1.AddDays(daysOffset);
+            int firstWeek = ci.Calendar.GetWeekOfYear(jan1, ci.DateTimeFormat.CalendarWeekRule, ci.DateTimeFormat.FirstDayOfWeek);
+            if ((firstWeek <= 1 || firstWeek >= 52) && daysOffset >= -3)
+            {
+                weekOfYear -= 1;
+            }
+            return firstWeekDay.AddDays(weekOfYear * 7);
+        }
+    }
+}
---
+++ Piforatio.Core/Piforatio.Core2Test/DateTimeWeekTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Piforatio.Core2;
+
+namespace Piforatio.Core2Test
+{
+    [TestFixture]
+    class DateTimeWeekTest
+    {
+        [Test]
+        public void Get9WeekStartDates()
+        {
+            //Arrange
+            DateTime start;
+
+            //Act
+            start = WeekNumber.FirstDateOfWeek(2017, 9);
+
+            //Assert
+            Assert.AreEqual(new DateTime(2017, 02, 27).Date, start.Date);
+        }
+    }
+}
