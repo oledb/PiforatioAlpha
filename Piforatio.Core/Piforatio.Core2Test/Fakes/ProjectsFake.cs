aa86c674cec98536f35e637ff7a774e5584ba329
oledb <zf0qwe@gmail.com>
2017-03-06 16:02:46 +0300

    Add ProjectsFake

---
+++ Piforatio.Core/Piforatio.Core2Test/Fakes/ProjectsFake.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Piforatio.Core2;
+
+namespace Piforatio.Core2Test.Fakes
+{
+    public static class ProjectsFake
+    {
+        public static Projects Create()
+        {
+            var fake = new Projects();
+            int index = 0;
+            fake.Add(new Project()
+            {
+                ID = index++,
+                Name = "MVC",
+                Type = ProjectType.Learn,
+                Comment = "Learn MVC, frontend and backend"
+            });
+            fake.Add(new Project()
+            {
+                ID = index++,
+                Name = "Upwork",
+                Type = ProjectType.Work,
+                Comment = "Carry out orders"
+            });
+
+            return fake;
+        }
+    }
+}
