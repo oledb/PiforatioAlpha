93bc9f2f3966b0ee7b755981647870e767e659bd
oledb <zf0qwe@gmail.com>
2017-03-17 12:54:07 +0300

    Create Alarmclock

---
+++ Piforatio.Core/Piforatio.Core2/Alarmclock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Piforatio.Core2
+{
+    public class Alarmclock
+    {
+        public Alarmclock()
+        {
+        }
+
+        private DateTime _now;
+        public double TotalSeconds { get; private set; }
+        
+        public void Start(DateTime now)
+        {
+            _now = now;
+        }
+
+        public void Stop(DateTime now)
+        {
+            TotalSeconds = (now - _now).TotalSeconds; ;
+        }
+    }
+}
---
+++ Piforatio.Core/Piforatio.Core2Test/AlarmclockTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Piforatio.Core2;
+using System;
+using Moq;
+
+namespace Piforatio.Core2Test
+{
+    [TestFixture]
+    class AlarmclockTest
+    {
+        DateTime today;
+
+        [SetUp]
+        public void SetToday()
+        {
+           today = new DateTime(2017, 3, 3, 13, 20, 00);
+        }
+
+        [TestCase(1)]
+        [TestCase(5)]
+        [TestCase(100000)]
+        public void StartAndStopTimerForXSeconds(int wait)
+        {
+            //Arrange
+            Alarmclock clock = new Alarmclock();
+
+            //Act
+            clock.Start(today);
+            clock.Stop(today.AddSeconds(wait));
+            var totalSeconds = clock.TotalSeconds;
+
+            //Assert
+            Assert.AreEqual(wait, totalSeconds);
+        }
+
+        public void StartGetTotalSeconds()
+        {
+
+        }
+    }
+}
