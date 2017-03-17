c0977b720b1010c28cf5a0963ba73c9112773e4e
oledb <zf0qwe@gmail.com>
2017-03-17 15:58:33 +0300

    Add code for Alarmclock

--- Piforatio.Core/Piforatio.Core2/Alarmclock.cs
+++ Piforatio.Core/Piforatio.Core2/Alarmclock.cs
@@ -4,21 +4,81 @@ namespace Piforatio.Core2
 {
     public class Alarmclock
     {
-        public Alarmclock()
+        private DateTime _StartTime;
+        private bool _isRun = false;
+        private bool _isPause = false;
+        private bool _isWaitable = false;
+        private double _totalTime;
+        private double _waitTime;
+
+        public bool IsRun
         {
+            get { return _isRun; }
         }
 
-        private DateTime _now;
-        public double TotalSeconds { get; private set; }
+        public double TotalSeconds
+        {
+            get
+            {
+                if (_isRun)
+                    return _totalTime;
+                else
+                    return 0;
+            }
+        }
         
+        public double WaitSecodns
+        {
+            get
+            {
+                if (_isWaitable)
+                    return _waitTime - _totalTime;
+                else
+                    return 0;
+            }
+        }
+
+        public void Pause(DateTime now)
+        {
+            if (_isPause)
+                return;
+            setTotalSeconds(now);
+            _isPause = true;
+        }
+
         public void Start(DateTime now)
         {
-            _now = now;
+            if (!_isPause)
+                _StartTime = now;
+            _isRun = true;
+            _isPause = false;
         }
 
-        public void Stop(DateTime now)
+        public void Start(DateTime today, double wait)
         {
-            TotalSeconds = (now - _now).TotalSeconds; ;
+            _waitTime = wait;
+            _isWaitable = true;
+            Start(today);
+        }
+
+        public void Reset()
+        {
+            _isRun = false;
+            _isPause = false;
+        }
+
+        private void setTotalSeconds(DateTime now)
+        {
+            _totalTime = (now - _StartTime).TotalSeconds;
+        }
+
+        public void Execute(DateTime now)
+        {
+            if (_isRun && _isPause)
+                return;
+            setTotalSeconds(now);
+            if (_isWaitable && WaitSecodns <= 0)
+                Reset();
         }
     }
 }
--- Piforatio.Core/Piforatio.Core2Test/AlarmclockTest.cs
+++ Piforatio.Core/Piforatio.Core2Test/AlarmclockTest.cs
@@ -1,7 +1,6 @@
 ﻿using NUnit.Framework;
 using Piforatio.Core2;
 using System;
-using Moq;
 
 namespace Piforatio.Core2Test
 {
@@ -18,24 +17,115 @@ namespace Piforatio.Core2Test
 
         [TestCase(1)]
         [TestCase(5)]
-        [TestCase(100000)]
-        public void StartAndStopTimerForXSeconds(int wait)
+        public void StartAndStopTimer(double wait)
         {
             //Arrange
             Alarmclock clock = new Alarmclock();
 
             //Act
             clock.Start(today);
-            clock.Stop(today.AddSeconds(wait));
+            clock.Execute(today);
+            clock.Reset();
+            clock.Execute(Wait(wait));
+            var totalSeconds = clock.TotalSeconds;
+
+            //Assert
+            Assert.AreEqual(0, totalSeconds);
+        }
+
+        [TestCase(1)]
+        [TestCase(100000)]
+        public void StartAndGetTotalSeconds(double wait)
+        {
+            //Arrange
+            Alarmclock clock = new Alarmclock();
+
+            //Act
+            clock.Start(today);
+            clock.Execute(Wait(wait));
             var totalSeconds = clock.TotalSeconds;
 
             //Assert
             Assert.AreEqual(wait, totalSeconds);
         }
 
-        public void StartGetTotalSeconds()
+        [TestCase(1)]
+        [TestCase(100)]
+        public void StartAndPause(double wait)
         {
+            //Arrange
+            Alarmclock clock = new Alarmclock();
 
+            //Act
+            clock.Start(today);
+            today = Wait(wait);
+            clock.Pause(today);
+            var a = clock.TotalSeconds;
+            today = Wait(wait);
+            var b = clock.TotalSeconds;
+
+            //Assert
+            Assert.AreEqual(a, b);
+        }
+
+        [TestCase(1)]
+        [TestCase(100)]
+        public void StartAndPauseAndExecuteSomeTimes(double wait)
+        {
+            //Arrange
+            Alarmclock clock = new Alarmclock();
+            clock.Start(today);
+
+            //Act
+            clock.Execute(Wait(wait));
+            clock.Pause(Wait(wait));
+            // Wait and press Pause againg.
+            clock.Execute(Wait(wait * 2));
+            clock.Pause(Wait(wait * 2));
+            clock.Execute(Wait(wait * 4));
+            var result = clock.TotalSeconds;
+
+            //Assert
+            Assert.AreEqual(wait, result);
+        }
+
+        public void StartAndPauseSomeTimes(double wait)
+        {
+            //Arrange
+            Alarmclock clock = new Alarmclock();
+            clock.Start(today);
+
+            //Arrange
+            clock.Pause(Wait(wait * 1));
+            clock.Start(Wait(wait * 2));
+            clock.Pause(Wait(wait * 4));
+            var result = clock.TotalSeconds;
+
+            //Assert
+            Assert.AreEqual(wait * 3, result);
+        }
+
+        [TestCase(3600)]
+        public void StartAndWaitWhenStop(double wait)
+        {
+            //Arrange
+            Alarmclock clock = new Alarmclock();
+
+            //Act
+            clock.Start(today, wait);
+            bool run = clock.IsRun;
+            today = Wait(wait);
+            clock.Execute(today);
+            bool stop = clock.IsRun;
+
+            //Assert
+            Assert.IsTrue(run);
+            Assert.IsFalse(stop);
+        }
+
+        public DateTime Wait(double count)
+        {
+            return today.AddSeconds(count);
         }
     }
 }
