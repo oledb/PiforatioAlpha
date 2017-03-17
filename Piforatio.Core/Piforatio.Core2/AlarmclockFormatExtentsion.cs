5ea971e78390997e4a9c8935e0b9d1bc3c0bef1f
oledb <zf0qwe@gmail.com>
2017-03-17 17:42:06 +0300

    Add AlarmclockFormatExtentsion

---
+++ Piforatio.Core/Piforatio.Core2/AlarmclockFormatExtentsion.cs
@@ -0,0 +1,10 @@
+﻿namespace Piforatio.Core2
+{
+    public static class AlarmclockFormatExtentsion
+    {
+        public static int ToMinute(this double seconds)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Piforatio.Core/Piforatio.Core2Test/AlarmclockFormatTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Piforatio.Core2;
+using System;
+
+namespace Piforatio.Core2Test
+{
+    [TestFixture]
+    class AlarmclockFormatTest
+    {
+        [TestCase(0d,0)]
+        [TestCase(60d, 1)]
+        public void ConvertSecondsToMinute(double seconds, int minute)
+        {
+            //Arrange,Act,Assert
+            Assert.AreEqual(minute, seconds.ToMinute());
+        }
+    }
+}
