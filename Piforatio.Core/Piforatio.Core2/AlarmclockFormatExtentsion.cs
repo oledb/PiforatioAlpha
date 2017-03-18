57ea4acb4996696b5bed2c7d2ccd487933d506a2
oledb <zf0qwe@gmail.com>
2017-03-18 19:21:13 +0300

    Add code to AlarmclockFormatExtentsion class

--- Piforatio.Core/Piforatio.Core2/AlarmclockFormatExtentsion.cs
+++ Piforatio.Core/Piforatio.Core2/AlarmclockFormatExtentsion.cs
@@ -1,10 +1,39 @@
-﻿namespace Piforatio.Core2
+﻿using static System.Math;
+using static System.Convert;
+
+namespace Piforatio.Core2
 {
     public static class AlarmclockFormatExtentsion
     {
-        public static int ToMinute(this double seconds)
+        public static int ToMinutes(this double total)
         {
-            return 0;
+            return ToInt32(Truncate(total / 60));
+        }
+
+        public static int ToHours(this double total)
+        {
+            return ToInt32(Truncate(total / 3600));
+        }
+
+        public static int ToSeconds(this double total)
+        {
+            return ToInt32(total) % 60;
+        }
+
+        public static string ToTimerFormat(this double total)
+        {
+            return string.Format("{0}:{1}:{2}",
+                total.ToHours().ToDigit(), 
+                total.ToMinutes().ToDigit(), 
+                total.ToSeconds().ToDigit());
+        }
+
+        private static string ToDigit(this int value)
+        {
+            var temp = value.ToString();
+            if (temp.Length == 1)
+                temp = "0" + temp;
+            return temp;
         }
     }
 }
--- Piforatio.Core/Piforatio.Core2Test/AlarmclockFormatTest.cs
+++ Piforatio.Core/Piforatio.Core2Test/AlarmclockFormatTest.cs
@@ -7,12 +7,42 @@ namespace Piforatio.Core2Test
     [TestFixture]
     class AlarmclockFormatTest
     {
-        [TestCase(0d,0)]
+        [TestCase(59d, 0)]
         [TestCase(60d, 1)]
-        public void ConvertSecondsToMinute(double seconds, int minute)
+        [TestCase(119d, 1)]
+        public void ConvertTotalSecondsToMinute(double total, int minutes)
         {
-            //Arrange,Act,Assert
-            Assert.AreEqual(minute, seconds.ToMinute());
+            //Arrange, Act, Assert
+            Assert.AreEqual(minutes, total.ToMinutes());
+        }
+
+        [TestCase(3599d, 0)]
+        [TestCase(3600d, 1)]
+        [TestCase(7199d, 1)]
+        public void ConvertTotalSecondsToHour(double total, int hours)
+        {
+            //Arrange, Act, Assert
+            Assert.AreEqual(hours, total.ToHours());
+        }
+
+        [TestCase(59d, 59)]
+        [TestCase(60d, 0)]
+        [TestCase(61d, 1)]
+        public void ConvertTotalSecondsToSeconds(double total, int seconds)
+        {
+            //Arrange, Act, Assert
+            Assert.AreEqual(seconds, total.ToSeconds());
+        }
+
+        [TestCase(59d, "00:00:59")]
+        [TestCase(119d, "00:01:59")]
+        public void ConvertTotalSecodsToTimerString(double total, string format)
+        {
+            //Arrange, Act
+            string result = total.ToTimerFormat();
+
+            //Assert
+            Assert.AreEqual(format, result);
         }
     }
 }
