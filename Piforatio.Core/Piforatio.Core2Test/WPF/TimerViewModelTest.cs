99fb6cbc1d116459a68eeb255c26ec6ef62274ce
oledb <zf0qwe@gmail.com>
2017-03-22 15:29:44 +0300

    Create timerViewModel

---
+++ Piforatio.Core/Piforatio.Core2Test/WPF/TimerViewModelTest.cs
@@ -0,0 +1,19 @@
+﻿using Piforatio.WPF;
+using NUnit.Framework;
+
+namespace Piforatio.Core2Test.WPF
+{
+    [TestFixture]
+    class TimerViewModelTest
+    {
+        [Test]
+        public void CreateTimerViewModel()
+        {
+            //Arrange, Act
+            TimerViewModel timer = new TimerViewModel();
+
+            //Assert
+            Assert.AreEqual("0000", timer.EndYear);
+        }
+    }
+}
