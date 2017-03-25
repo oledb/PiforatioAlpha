6bf4496749103b47057f1fad3962e83c03aead02
oledb <zf0qwe@gmail.com>
2017-03-25 11:47:30 +0300

    Changed basic design of MainWindow

--- Piforatio.Core/Piforatio.Core2Test/WPF/TimerViewModelTest.cs
+++ Piforatio.Core/Piforatio.Core2Test/WPF/TimerViewModelTest.cs
@@ -1,4 +1,5 @@
-﻿using Piforatio.WPF;
+﻿using System;
+using Piforatio.WPF;
 using NUnit.Framework;
 
 namespace Piforatio.Core2Test.WPF
@@ -6,14 +7,67 @@ namespace Piforatio.Core2Test.WPF
     [TestFixture]
     class TimerViewModelTest
     {
-        [Test]
-        public void CreateTimerViewModel()
+                  //3.22.2017 6.16pm
+        [TestCase(636258034072347780, "6822")]
+                  //3.23.2017 4.34pm
+        [TestCase(636258836307637505, "6799")]
+        public void CreateTimerViewModel(long today, string endYearResult)
         {
             //Arrange, Act
-            TimerViewModel timer = new TimerViewModel();
+            var time = new DateTime(today, DateTimeKind.Local);
+            IDateTime dateTime = new TodayStub(time);
+            TimerViewModel timer = new TimerViewModel(dateTime);
 
             //Assert
-            Assert.AreEqual("0000", timer.EndYear);
+            Assert.AreEqual(endYearResult, timer.EndYear);
+            Assert.AreEqual("00:00:00", timer.TimeWork);
+            Assert.IsNull(timer.TimePause);
+            Assert.IsFalse(timer.IsStarted);
+            Assert.IsNull(timer.CurrentObjective);
+        }
+
+        [Test]
+        public void StartTimerViewModel()
+        {
+            //Arrange
+            var time = new DateTime(636258836307637505, DateTimeKind.Local);
+            IDateTime dateTime = new TodayStub(time);
+            TimerViewModel timer = new TimerViewModel(dateTime);
+
+            //Act
+            //timer.Start();
+            //timer.Execute();
+
+        }
+    }
+
+    public class TodayStub : IDateTime
+    {
+        protected DateTime _now;
+        public TodayStub(DateTime now)
+        {
+            _now = now;
+        }
+
+        public virtual DateTime Now
+        {
+            get { return _now; }
+        }
+    }
+
+    public class Today1SecFake : TodayStub
+    {
+        private int _index;
+
+        public Today1SecFake(DateTime now) : base(now) { }
+
+        public override DateTime Now
+        {
+            get
+            {
+                _index++;
+                return _now.AddSeconds(_index);
+            }
         }
     }
 }
--- Piforatio.Core/Piforatio.WPF/TimerViewModel.cs
+++ Piforatio.Core/Piforatio.WPF/TimerViewModel.cs
@@ -1,12 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Piforatio.Core2;
 
 namespace Piforatio.WPF
 {
     public class TimerViewModel
     {
+        IDateTime _dateTime;
+
+        public TimerViewModel(IDateTime dateTime)
+        {
+            _dateTime = dateTime;
+        }
+
+        public object CurrentObjective
+        {
+            get { return null; }
+        }
+
+        public string EndYear
+        {
+            get { return getEndYear(); }
+        }
+
+        public bool IsStarted { get; private set; }
+
+        public object TimePause
+        {
+            get { return null; }
+        }
+
+        public string TimeWork
+        {
+            get { return "00:00:00"; }
+        }
+
+        private string getEndYear()
+        {
+            var date = _dateTime.Now;
+            var nextYear = date.Year + 1;
+            var nextDate = new DateTime(nextYear, 1, 1, 0, 0, 0);
+            var totalHours = (nextDate - date).TotalHours;
+            return Convert.ToInt32(totalHours).ToString() ;
+        }
     }
 }
